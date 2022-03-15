230e6e8a98d2b41fa7e39dd1c3bc7187eb9189bd
Rolton22 <GITHUB rostislavkaraman@gmail.com>
2022-03-15 22:05:43 +0200

    Initial commit

---
+++ src/com/company/Pixel.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Pixel {
+
+
+    public static void main(String[] args) {
+        String Default = "\u001B[0m";
+        String Black = "\u001b[48;5;16m";
+        String White = "\u001b[48;5;15m";
+        String Yellow = "\u001b[48;5;220m";
+        String Red = "\u001b[48;5;196m";
+        System.out.println(White + "   " + Black + "      " + White + "                                                                        " + Black + "      " + White + "   " + Default);
+        System.out.println(White + "   " + Black + "            " + White + "                                                            " + Black + "            " + White + "   " + Default);
+        System.out.println(White + "   " + Black + "         " + Yellow + "   " + Black + "   " + White + "                                                      " + Black + "   " + Yellow+ "   " + Black + "         " + White + "   " + Default);
+        System.out.println(White + "   " + Black + "         " + Yellow + "      "+ Black + "      " + White + "                                          " + Black + "      " + Yellow + "      " + Black + "         " + White + "   " + Default);
+        System.out.println(White + "      " + Black + "      " + Yellow + "            " + Black + "   " + White + "                                    " + Black + "   " + Yellow + "            " + Black + "      " + White + "      " + Default);
+        System.out.println(White + "      " + Black + "      " + Yellow + "               " + Black + "      " + White + "      " + Black + "            " + White + "      " + Black + "      " + Yellow + "               " + Black + "      " + White + "      " + Default);
+        System.out.println(White + "         " + Black + "   " + Yellow + "                     " + Black + "      " + Yellow + "            " + Black + "      " + Yellow + "                     " + Black + "   " + White + "         " + Default);
+        System.out.println(White + "         " + Black + "   " + Yellow + "                                                                  " + Black + "   " + White + "         " + Default);
+        System.out.println(White + "            " + Black + "   " + Yellow + "                                                            " + Black + "   " + White + "            " + Default);
+        System.out.println(White + "               " + Black + "   " + Yellow + "   " + Black + "   " + Yellow + "                                          " + Black + "   " + Yellow + "   " + Black + "   " + White + "               " + Default);
+        System.out.println(White + "                  " + Black + "   " + Yellow + "                                                " + Black + "   " + White + "                  " + Default);
+        System.out.println(White + "                  " + Black + "   " + Yellow + "                                                " + Black + "   " + White + "                  " + Default);
+        System.out.println(White + "               " + Black + "   " + Yellow + "         " + Black + "      " + Yellow + "                        " + Black + "      " + Yellow + "         " + Black + "   " + White + "               " + Default);
+        System.out.println(White + "               " + Black + "   " + Yellow + "      " + Black + "   " + White + "   " + Black + "      " + Yellow + "                  " + Black + "      " + White + "   " + Black + "   " + Yellow + "      " + Black + "   " + White + "               " + Default);
+        System.out.println(White + "               " + Black + "   " + Yellow + "      " + Black + "            " + Yellow + "                  " + Black + "            " + Yellow + "      " + Black + "   " + White + "               " + Default);
+        System.out.println(White + "               " + Black + "   " + Yellow + "         " + Black + "      " + Yellow + "         " + Black + "      " + Yellow + "         " + Black + "      " + Yellow + "         " + Black + "   " + White + "               " + Default);
+        System.out.println(White + "            " + Black + "   " + Yellow + "      " + Red + "      " + Yellow + "                                    " + Red + "      " + Yellow + "      " + Black + "   " + White + "            " + Default);
+        System.out.println(White + "            " + Black + "   " + Yellow + "   " + Red + "            " + Yellow + "      " + Black + "   " + Yellow + "   " + Black + "      " + Yellow + "   " + Black + "   " + Yellow + "      " + Red + "            " + Yellow + "   " + Black + "   " + White + "            " + Default);
+        System.out.println(White + "            " + Black + "   " + Yellow + "   " + Red + "            " + Yellow + "         " + Black + "            " + Yellow + "         " + Red + "            " + Yellow + "   " + Black + "   " + White + "            " + Default);
+        System.out.println(White + "               " + Black + "   " + Yellow + "   " + Red + "      " + Yellow + "            " + Black + "   " + Red + "      " + Black + "   " + Yellow + "            " + Red + "      " + Yellow + "   " + Black + "   " + White + "               " + Default);
+        System.out.println(White + "                  " + Black + "   " + Yellow + "                  " + Black + "   " +  Red + "      " + Black + "   " + Yellow + "                  " + Black + "   " + White + "                  " + Default);
+        System.out.println(White + "                     " + Black + "      " + Yellow + "               " + Black + "      " + Yellow + "               " + Black + "      " + White + "                     " + Default);
+        System.out.println(White + "                           " + Black + "         " + Yellow + "                  " + Black + "         " + White + "                           " + Default);
+        System.out.println(White + "                                    " + Black + "                  " + White + "                                    " + Default);
+    }
+}
